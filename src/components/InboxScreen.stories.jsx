1dde4ef2f2428907e2707076bcdcc7350256e46c
Steve Phelps <79sphelps@gmail.com>
2024-04-29 10:21:27 -0700

    adding composite component, data, and screen examples

---
+++ src/components/InboxScreen.stories.jsx
@@ -0,0 +1,86 @@
+import InboxScreen from './InboxScreen';
+import store from '../lib/store';
+//  import { rest } from 'msw';
+import { http, HttpResponse } from 'msw';
+ import { MockedState } from './TaskList.stories';
+import { Provider } from 'react-redux';
+
+import {
+  fireEvent,
+ waitFor,
+ within,
+ waitForElementToBeRemoved
+ } from '@storybook/test';
+
+export default {
+  component: InboxScreen,
+  title: 'InboxScreen',
+  decorators: [(story) => <Provider store={store}>{story()}</Provider>],
+  tags: ['autodocs'],
+};
+
+export const Default = {
+ parameters: {
+   msw: {
+     handlers: [
+      //  rest.get(
+      //    'https://jsonplaceholder.typicode.com/todos?userId=1',
+      //    (req, res, ctx) => {
+      //      return res(ctx.json(MockedState.tasks));
+      //    }
+      //  ),
+      http.get('https://jsonplaceholder.typicode.com/todos?userId=1', ({ request, params, cookies }) => {
+        return HttpResponse.json(
+          {
+            message: 'Mocked response',
+            // message: MockedState.tasks[1].title
+          },
+          {
+            status: 202,
+            statusText: 'Mocked status',
+          },
+        )
+      })
+     ],
+   },
+ },
+ play: async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  // Waits for the component to transition from the loading state
+  await waitForElementToBeRemoved(await canvas.findByTestId('loading'));
+  // Waits for the component to be updated based on the store
+  await waitFor(async () => {
+  // Simulates pinning the first task
+  await fireEvent.click(canvas.getByLabelText('pinTask-1'));
+  // Simulates pinning the third task
+  await fireEvent.click(canvas.getByLabelText('pinTask-3'));
+  });
+  },
+};
+export const Error = {
+ parameters: {
+   msw: {
+     handlers: [
+      //  rest.get(
+      //    'https://jsonplaceholder.typicode.com/todos?userId=1',
+      //    (req, res, ctx) => {
+      //      return res(ctx.status(403));
+      //    }
+      //  ),
+      http.get('https://jsonplaceholder.typicode.com/todos?userId=1', ({ request, params, cookies }) => {
+        return HttpResponse.json(
+          {
+            message: 'Mocked response',
+            // message: MockedState.tasks[1].title
+          },
+          {
+            status: 403,
+            statusText: 'Mocked status',
+          },
+        )
+      })
+
+     ],
+   },
+ },
+};
